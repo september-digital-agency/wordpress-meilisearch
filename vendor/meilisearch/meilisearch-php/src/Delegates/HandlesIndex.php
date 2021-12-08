a83ddbf05570eb937cb3a8ce63890580d75fcc61
Daan Meijer <daan@studioseptember.nl>
2021-12-08 15:45:54 +0100

    add metadata to posts when searching with meili

--- vendor/meilisearch/meilisearch-php/src/Delegates/HandlesIndex.php
+++ vendor/meilisearch/meilisearch-php/src/Delegates/HandlesIndex.php
@@ -61,7 +61,7 @@ trait HandlesIndex
         try {
             $index = $this->getIndex($uid);
         } catch (ApiException $e) {
-            if (\is_array($e->httpBody) && 'index_not_found' === $e->httpBody['errorCode']) {
+            if ($e->httpStatus == 404) {
                 $index = $this->createIndex($uid, $options);
             } else {
                 throw $e;
