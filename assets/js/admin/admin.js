1106b3b5d7704e60f1b2488a2c496c56293162c5
Daan Meijer <daan@studioseptember.nl>
2022-02-09 09:47:07 +0100

    foutmelding bij ontbrekend element voorkomen

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -2,9 +2,12 @@ function fetchStats() {
 	fetch("admin-ajax.php?action=stats")
 		.then((response) => response.json())
 		.then((data) => {
-			document.querySelector(
-				".wordpress-meilisearch-realtime-numberOfDocuments"
-			).innerHTML = data.numberOfDocuments;
+
+			const elm = document.querySelector(".wordpress-meilisearch-realtime-numberOfDocuments");
+
+			if(elm){
+				elm.innerHTML = data.numberOfDocuments;
+			}
 
 			if (data.isIndexing == true) {
 				setTimeout(fetchStats, 500);
